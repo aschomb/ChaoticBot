cc635bb785955cf91127b9090c1ce191b5ec51be
root <root@localhost.localdomain>
2021-07-06 12:27:56 -0400

    banlist command and fixed ban/unban

--- bot/cogs/moderation.py
+++ bot/cogs/moderation.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from dpytools.checks import *
 ecolor = 0xe91e63
 
 class moderation(commands.Cog):
@@ -7,34 +8,67 @@ class moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-
-    @commands.command()
     @commands.has_permissions(kick_members=True)
+    @commands.is_owner()
+    @commands.command()
     async def kick(self, ctx, member: discord.Member, *, reason = None):
         await member.kick(reason=reason)
-        await ctx.send(f'User {member} has been kicked for {reason}.')
-
-    @commands.command()
+        #await ctx.send(f'User {member} has been kicked for {reason}.')
+        em = discord.Embed(title=f"**Kicked {member.display_name}!**", description= f"Reason:{reason}\nBy: {ctx.author.display_name}", color = ecolor)
+        await ctx.send(embed = em)
+        await member.send(embed = em)
+    
     @commands.has_permissions(ban_members=True)
+    @commands.is_owner()
+    @commands.command()
     async def ban(self, ctx, member: discord.Member, *, reason = "No reason provided"):
         await member.ban(reason = reason)
-        ban = discord.Embed(title=f"Banned {member.display_name}!", description = f"Reason: {reason}\nBy: {ctx.author.mention}", color=ecolor)
-        await ctx.channel.send(embed=ban)
+        ban = discord.Embed(title=f"**Banned {member.display_name}!**", description = f"Reason: {reason}\nBy: {ctx.author.mention}", color=ecolor)
+        await ctx.send(embed=ban)
+        await member.send(embed = ban)
+    
+    @commands.has_permissions(ban_members=True)
+    @commands.is_owner()
+    @commands.command()
+    async def banlist(self, ctx):
+        banned_users = await ctx.guild.bans()
+        for ban in banned_users:
+            #await ctx.send(ban)
+            #em = discord.Embed(title="**Ban List**",color=ecolor)
+            #em.add_field(name="**Users**", value=ban[1].name + '#'  + ban[1].discriminator, inline=True)
+            #em.add_field(name="**ID**", value=str(ban[1].id), inline=True)
+            await ctx.send("User: " + ban[1].name + '#' + ban[1].discriminator + '\nID: ' + str(ban[1].id) + '\nReason: ' + ban.reason)
+        #await ctx.send(embed = em)    
 
     @commands.command()
+    async def banEntry(self, ctx, *, member):
+        #await ctx.send(member)
+        banned_users = await ctx.guild.bans()
+        for ban_entry in banned_users:
+            user = ban_entry.user
+            #member_name, member_disc = member.split('#')
+            #await ctx.send(member_name + '#' + member_disc)
+            await ctx.send(user)
+
     @commands.has_permissions(ban_members=True)
+    @commands.is_owner()
+    @commands.command()
     async def unban(self, ctx, *, member):
         banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split('#')
+        member_name, member_discriminator, *_ = member.split('#') + [None]
         for ban_entry in banned_users:
             user = ban_entry.user
             if (user.name, user.discriminator) == (member_name, member_discriminator):
+            #if (user.id == member.id):
                 await ctx.guild.unban(user)
                 await ctx.send(f'Unbanned {user.mention}')
+                await member.send(f"{member.mention}, you have been unbanned by {ctx.author.display_name}")
                 return
+        await ctx.send("User was not banned/found.")
 
     @commands.command()
-    @commands.is_owner() 
+    @commands.is_owner()
+    @only_these_users(736309573924683917,290926756397842432)
     async def addrole(self, ctx, member: discord.Member, role: discord.Role):
         try:
             await member.add_roles(role)
@@ -44,6 +78,7 @@ class moderation(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
+    @only_these_users(736309573924683917,290926756397842432)
     async def removerole(self, ctx, member: discord.Member, role: discord.Role):
         try:
             await member.remove_roles(role)
@@ -52,11 +87,13 @@ class moderation(commands.Cog):
             await ctx.send("Error removing role.")
 
     @commands.command()
+    @only_these_users(736309573924683917,290926756397842432)
     async def dm(self, ctx, user: discord.User, *, msg):
         await ctx.send('Successful.')
         await user.send(f'{msg}')
 
     @commands.command()
+    @only_these_users(736309573924683917,290926756397842432)
     async def announce(self, ctx, channel: discord.TextChannel, *, title, message):
         em = discord.Embed(description = f"{message}")
         await channel.send(embed = em)
@@ -65,10 +102,24 @@ class moderation(commands.Cog):
     async def on_message(self, message):
         slurs = ["fag","faggot","nigger","nigga"]
         msg = message.content
+        user = message.author
         if any(word in msg.lower() for word in slurs):
             await message.delete()
-            await message.channel.send(f"{message.author.mention}, you can't say that.")
+            em = discord.Embed(title="Message deleted",color=ecolor)
+            em.add_field(name="**Warning**",value=f"You can't say that word",inline=True)
+            em.add_field(name="**Message Content**",value=f"{message.content}", inline=True)
+            #em.add_field(name="**Disclaimer**",value="You may not have intended for your message to contain a slur, if this was a mistake, ignore it.", inline=True)
+            em.set_footer(text="Disclaimer: not every deleted message purposely contained a slur, the bot just happened to pick it up.  If that is the case, just ignore this.")
+            await user.send(embed=em)
+            #await user.send(f"{message.author.mention}, you can't say that.{message.content}")
+            #await user.send(f"{message.content}")
+            #await message.channel.send(f"{message.author.mention}, you can't say that.")
 
+    #@commands.command()
+    #async def mute(self, ctx, member: discord.Member, *, reason = None):
+    #    text_channel_list = []
+    #    for guild in client.guilds:
+    #        for channel in guild.text_channels:
     #@commands.command()
     #async def mute(self, ctx, member: discord.Member, *, reason = None):
     #    text_channel_list = []
@@ -92,8 +143,6 @@ class moderation(commands.Cog):
     #    for channel in text_channel_list:
     #        await ctx.channel.set_permissions(member, send_messages=True)
     #
-    #    mute = discord.Embed(title=f"Unmuted {member.display_name}!", description = f"By: {ctx.author.mention}")
-    #    await ctx.channel.send(embed=mute)
 
 def setup(client):
     client.add_cog(moderation(client))
